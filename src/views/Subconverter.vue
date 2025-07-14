95967fc86c96f179695b057f411b108f7602ebe1
DawnLi <51946856+dawnli139@users.noreply.github.com>
2025-07-14 17:01:53 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,24 +338,21 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
-          "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
-          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
+          "Dawn Li Backend": "https://subapi.dawnli.me",
         },
         backendOptions: [
-          { value: "https://subapi.cmliussss.net" },
-          { value: "https://subapi.fxxk.dedyn.io" },
-          { value: "https://url.v1.mk" },
-          { value: "https://sub.d1.mk" },
-          { value: "https://www.nameless13.com" },
-          { value: "https://sub.xeton.dev" },
-          { value: "https://api.wcc.best" },
+          { value: "https://subapi.dawnli.me" },
         ],
         remoteConfig: [
+          {
+            label: "AI规则",
+            options: [
+              {
+                label: "ClashRuleSet AI分流",
+                value: "https://raw.githubusercontent.com/szkane/ClashRuleSet/main/Clash/kclash.ini"
+              }
+            ]
+          },
           {
             label: "CM规则",
             options: [
